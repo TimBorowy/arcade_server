d64a3aeb31371bbf2d96b19375e75fae715f4772
Tim Borowy <blastertimbo@gmail.com>
2019-03-25 16:28:08 +0100

    manifest.json supplies info for how to play screen

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,4 +1,4 @@
-
+let request = require('request')
 
 let controller = {
 
@@ -6,7 +6,21 @@ let controller = {
         res.render('index', {})
     },
     howToPlay: function (req, res) {
-        res.render('buttons', {})
+
+        // get game title from url params
+        let gameTitle = req.param('game')
+        let response = res
+
+        // make get request to manifest.json
+        // Todo: make url less hardcoded
+        request('http://localhost:8080/' + gameTitle + '/manifest.json', { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+
+            let manifest = body
+
+            // render template with data from manifest
+            response.render('buttons', { manifest })
+        })
     }
 }
 module.exports = controller
